f997cf56fbb781bd041f21e0942fec70a55caa62
abdurcodes <aravman@gmail.com>
2024-09-19 18:07:20 +0500

    set up the initial skeleton of the project

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const myLibrary = [];
+
+function Book(author, title, numberOfPages, isRead) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
