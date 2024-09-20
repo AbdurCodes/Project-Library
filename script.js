a89e7d87f1e9ef82bc8651f2b88076286663009e
abdurcodes <aravman@gmail.com>
2024-09-20 09:55:34 +0500

    set up 'show all books' button and 'add new book' modal

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+let booksInLibrary = document.querySelector('.booksInLibrary');
 
-const myLibrary = [];
+let myLibrary = ['Sacrificed', 'Order of the Phoenix', 'Catching Fire', 'Afraid of the World', 'The book of Mirdad'];
 
 function Book(author, title, numberOfPages, isRead) {
     this.author = author;
@@ -9,6 +10,65 @@ function Book(author, title, numberOfPages, isRead) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
 }
 
+
+function showAllBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+
+        let book_card = document.createElement('div');
+        book_card.setAttribute('class', 'book-card');
+
+            let book_cover = document.createElement('div');
+            book_cover.setAttribute('class', 'book-cover');
+            book_card.appendChild(book_cover);
+
+            let book_title = document.createElement('h3');
+            book_title.setAttribute('class', 'book-title');
+            book_title.textContent = `${myLibrary[i]}`;
+            book_card.appendChild(book_title);
+
+            let book_actions = document.createElement('div');
+            book_actions.setAttribute('class', 'book-actions');
+            book_card.appendChild(book_actions);
+
+                let delete_button = document.createElement('button');
+                delete_button.setAttribute('class', 'delete-btn');
+                delete_button.textContent = 'Delete This Book';
+                book_actions.appendChild(delete_button);
+
+                let readThisBook = document.createElement('div');
+                readThisBook.setAttribute('class', 'readThisBook');
+                book_actions.appendChild(readThisBook);
+
+                    let inputElement  = document.createElement('input');
+                    inputElement.type = 'checkbox';
+                    inputElement.id = 'isRead';
+                    readThisBook.appendChild(inputElement);
+
+                    let labelElement = document.createElement('label');
+                    labelElement.for = 'isRead';
+                    labelElement.textContent = 'Have read this book';
+                    readThisBook.appendChild(labelElement);
+
+        booksInLibrary.appendChild(book_card);
+    }
+}
+
+
+
+// modal script
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#addNewBook");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
